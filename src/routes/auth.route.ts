3fa876bf50f6954920570b7bf037935124e16fcc
bigboyang <87364146+bigboyang@users.noreply.github.com>
2023-01-28 14:39:36 +0900

    jwt header

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,6 +5,9 @@ import ErrorException from '../exceptions/form.exception';
 import { badRequest, badData, unAuthorized } from '../exceptions/definition.exception';
 import { resSuccess, responseWrapper } from '../utils/handler';
 import { AuthorizationService } from 'src/services';
+import { verifyHeaders } from '../middlewares/authorizations.middleware';
+// const redisClient = require( '../utils/redis' );
+const jwtUtils = require( '../utils/jwt-utils' );
 
 const router = Router();
 
@@ -18,5 +21,37 @@ router.get( '/authorization/:userUuid', responseWrapper( async ( req: Request, r
   console.log( userUuid );
   resSuccess( res, { result: userUuid });
 }) );
+
+// 로그인 - 토큰 발급
+router.post( '/login', responseWrapper( async ( req: Request, res: Response ) => {
+  const { userId, password } = req.body;
+
+  if ( !userId || !password ) {
+    throw new ErrorException( badRequest );
+  }
+
+  // user-server에서 유저 정보 가져오기
+  const success = true;
+
+  if ( !success ) {
+    throw new ErrorException( unAuthorized );
+  }
+
+  const accessToken = jwtUtils.sign( req.body );
+  const refreshToken = jwtUtils.refresh();
+
+  res.header({ 'Authorization': accessToken, 'refresh': refreshToken });
+
+  // redisClient.set( userId, refreshToken ); // refresh token redis에 저장
+  resSuccess( res , { access_token: accessToken, refresh_token : refreshToken });
+}) );
+
+// 헤더 테스트
+router.get( '/header', verifyHeaders, responseWrapper( async ( req: Request, res: Response ) => {
+
+  console.log( 3 );
+  resSuccess( res, { result: req.body.userId });
+}) );
+
 
 export default router;
