c832d4071a9dde7277914d95f7a5337146ec1481
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-02-25 15:55:07 +0900

    fix: error code 시작 숫자 변경

--- src/exceptions/definition.exception.ts
+++ src/exceptions/definition.exception.ts
@@ -1,25 +1,25 @@
 import ExceptionAttribute from './attribute.exception';
 
 // 400
-export const badRequest = new ExceptionAttribute( 400, 80000, 'Bad request' );
-export const badData = new ExceptionAttribute( 400, 80001, 'Invalid data' ); 
-export const needUserUuid = new ExceptionAttribute( 400, 80005, 'Need userUuid' ); 
-export const upsertFail = new ExceptionAttribute( 400, 80006, 'Upsert fail' );
-export const updateFail = new ExceptionAttribute( 400, 80007, 'Update fail' );
-export const createFail = new ExceptionAttribute( 400, 80008, 'Create fail' );
-export const deleteFail = new ExceptionAttribute( 400, 80009, 'Delete fail' );
-export const notFoundData = new ExceptionAttribute( 400, 80100, 'Empty Data' );
+export const badRequest = new ExceptionAttribute( 400, 90000, 'Bad request' );
+export const badData = new ExceptionAttribute( 400, 90001, 'Invalid data' ); 
+export const needUserUuid = new ExceptionAttribute( 400, 90005, 'Need userUuid' ); 
+export const upsertFail = new ExceptionAttribute( 400, 90006, 'Upsert fail' );
+export const updateFail = new ExceptionAttribute( 400, 90007, 'Update fail' );
+export const createFail = new ExceptionAttribute( 400, 90008, 'Create fail' );
+export const deleteFail = new ExceptionAttribute( 400, 90009, 'Delete fail' );
+export const notFoundData = new ExceptionAttribute( 400, 90050, 'Empty Data' );
 
 // 401
-export const unAuthorized = new ExceptionAttribute( 401, 80100, 'Have no authorization' );
-export const unAuthorizedToken = new ExceptionAttribute( 400, 80101, 'Unauthorized Token' );
-export const expiredToken = new ExceptionAttribute( 400, 80101, 'expired Token' );
+export const unAuthorized = new ExceptionAttribute( 401, 90100, 'Have no authorization' );
+export const unAuthorizedToken = new ExceptionAttribute( 400, 90101, 'Unauthorized Token' );
+export const expiredToken = new ExceptionAttribute( 400, 90102, 'expired Token' );
 
 // 404
-export const notFoundRoute = new ExceptionAttribute( 404, 80400, 'Not found route' );
+export const notFoundRoute = new ExceptionAttribute( 404, 90400, 'Not found route' );
 
 // 500
-export const ServerErrorForLogout = new ExceptionAttribute( 500, 85001, 'Server Error' );
+export const ServerErrorForLogout = new ExceptionAttribute( 500, 95001, 'Server Error' );
 
 // 502
-export const econnrefused = new ExceptionAttribute( 502, 10502, 'Server connection refused' );
+export const econnrefused = new ExceptionAttribute( 502, 90502, 'Server connection refused' );
